bd3e11df221b5f485a22b03add5c994645519e21
Francesco Panciroli <francesco.panciroli@unibo.it>
2022-12-02 14:42:27 +0100

    feat: day 1

---
+++ day-01/part-2.mjs
@@ -0,0 +1,12 @@
+import fs from 'fs'
+
+const input = fs.readFileSync('./input.txt', 'utf-8')
+
+const elves = input.split(/\r?\n\r?\n/)
+
+const elvesScores = elves.map(elf => elf.split(/\r?\n/).reduce((acc, score) => acc + Number(score), 0))
+
+elvesScores.sort((a, b) => b - a)
+
+console.log(elvesScores[0] + elvesScores[1] + elvesScores[2])
+
