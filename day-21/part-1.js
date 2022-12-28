25f6f534a1ee51a0a5fecfd6d7f2199a3690069a
Francesco Panciroli <fpanciroli@gmail.com>
2022-12-28 22:10:53 +0100

    feat: day 21 (part 1)

---
+++ day-21/part-1.js
@@ -0,0 +1,73 @@
+const {readFileSync} = require('fs')
+
+const input = readFileSync('./input.txt', 'utf-8')
+
+const fns = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+  '/': (a, b) => a / b,
+}
+
+const parseInput = input => {
+  const leaves = []
+  const nodes = input.split(/\n/).reduce((acc, line) => {
+    const [name, cmd] = line.split(': ')
+    const node = {
+      name,
+      parents: []
+    }
+    if (cmd.indexOf(' ') > 0) {
+      const [left, opSymbol, right] = cmd.split(' ')
+      node.op = fns[opSymbol]
+      node.leftName = left
+      node.rightName = right
+    } else {
+      node.value = Number(cmd)
+      leaves.push(node)
+    }
+    acc[name] = node
+    return acc
+  }, {})
+  return { nodes, leaves }  
+}
+
+const makeLinks = nodes => {
+  for (const nodeName in nodes) {
+    const node = nodes[nodeName]
+    if (node.leftName) {
+      node.left = nodes[node.leftName]
+      nodes[node.leftName].parents.push(node)
+      node.right = nodes[node.rightName]
+      nodes[node.rightName].parents.push(node)
+    }
+  }
+}
+
+const calculateRoot = (nodes, leaves) => {
+  let node = leaves.shift()
+  while (node) {
+    const parentsCount = node.parents.length
+    let parentsSolved = 0
+    for (const parentNode of node.parents) {
+      if (!parentNode.value && parentNode.left.value && parentNode.right.value) {
+        parentNode.value = parentNode.op(parentNode.left.value, parentNode.right.value)
+        if (parentNode.name === 'root') {
+          return parentNode.value
+        }
+        leaves.push(parentNode)
+        parentsSolved++
+      }
+    }
+    if (parentsCount > parentsSolved) {
+      leaves.push(node)
+    }
+    node = leaves.shift()
+  }
+}
+
+const { nodes, leaves } = parseInput(input)
+makeLinks(nodes)
+const rootValue = calculateRoot(nodes, leaves)
+
+console.log(rootValue)
